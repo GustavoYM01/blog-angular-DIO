7bcedfd3ea105e0e1012e072252fdab273e4bda9
GustavoYM01 <gumatimoto@hotmail.com>
2023-10-03 15:21:06 -0300

    Add alterações

---
+++ src/data/dataFake.ts
@@ -0,0 +1,18 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Nova atualização do C# é lançada!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus fuga qui et. Non earum autem itaque modi sit. Quod placeat sit excepturi praesentium recusandae officia exercitationem optio nisi unde quisquam.',
+    photoCover:
+      'https://leandromtr.com/wp-content/uploads/2020/05/conceitos-basicos-do-csharp.jpg',
+  },
+  {
+    id: '2',
+    title: 'Java é contemplado com novas melhorias contra vulnerabilidades!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus fuga qui et. Non earum autem itaque modi sit. Quod placeat sit excepturi praesentium recusandae officia exercitationem optio nisi unde quisquam.',
+    photoCover:
+      'https://embarcados.com.br/wp-content/uploads/2014/03/imagem-de-destaque-28-850x510.png',
+  },
+];
